eacdc842b4d64fbbdde2139a391987703730f7fe
angus <angus_qing168@163.com>
2019-01-08 18:03:28 +0800

    nuget

--- Angus.ISoft.Boilerplate.Structuremapper/ControllerFactory.cs
+++ Angus.ISoft.Boilerplate.Structuremapper/ControllerFactory.cs
@@ -1,30 +1,24 @@
-﻿using StructureMap;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿//using StructureMap;
+//using System;
+//using System.Web.Mvc;
 
-namespace Angus.ISoft.Boilerplate.Structuremapper.MVC
-{
-    public class StructureMapControllerFactory : DefaultControllerFactory
-    {
-        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
-        {
+//namespace Angus.ISoft.Boilerplate.Structuremapper.MVC
+//{
+//    public class StructureMapControllerFactory : DefaultControllerFactory
+//    {
+//        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+//        {
+//            if (requestContext == null || controllerType == null) return null;
+//            return (Controller)ObjectFactory.Container.GetInstance(controllerType);
+//        }
+//    }
 
-            if (requestContext == null || controllerType == null) return null;
-
-            return (Controller)ObjectFactory.Container.GetInstance(controllerType);
-        }
-    }
-
-    public static class ServiceMapper
-    {
-        public static void Register<T>() where T : Registry, new()
-        {
-            ControllerBuilder.Current.SetControllerFactory(new StructureMapControllerFactory());
-            ObjectFactory.Initialize<T>();
-        }
-    }
-}
+//    public static class ServiceMapper
+//    {
+//        public static void Register<T>() where T : Registry, new()
+//        {
+//            ControllerBuilder.Current.SetControllerFactory(new StructureMapControllerFactory());
+//            ObjectFactory.Initialize<T>();
+//        }
+//    }
+//}
